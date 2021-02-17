b0ac67e6b74aaa881715901a0a98c640da2c7544
jonathanbarette <jonathan.barette@outlook.be>
2021-02-17 15:21:06 +0100

    Before refactoring, add some unit tests to cover UpdateQuality method in GildedRose class: - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality. - "Backstage passes" quality increases by 1 when there are more than 10 days. - "Backstage passes" quality increases by 2 when there are 10 days or less. - "Backstage passes" quality increases by 3 when there are 5 days or less. - "Backstage passes" quality drops to 0 after the concert.

--- GildedRose-Refactoring-KataTests/GildedRoseTests/UpdateQualityTests.cs
+++ GildedRose-Refactoring-KataTests/GildedRoseTests/UpdateQualityTests.cs
@@ -108,5 +108,127 @@ namespace GildedRoseRefactoringKataTests.GildedRoseTests
             //Assert
             Assert.IsTrue(items[0].Quality <= 50);
         }
+
+        /// <summary>
+        /// "Sulfuras", being a legendary item, never has to be sold or decreases in Quality.
+        /// </summary>
+        /// <param name="sellIn">A range of sellIn.</param>
+        /// <param name="quality">A set of values for quality.</param>
+        [Test]
+        public void GildedRose_UpdateQuality_SufurasProductNeverHasToBeSoldOrDecreasesInQuality(
+            [Range(-5, 5)] int sellIn,
+            [Values(10, 25, 50)] int quality)
+        {
+            //Arrange
+            var items = new List<Item>
+            {
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = sellIn, Quality = quality }
+            };
+
+            //Act
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            //Assert
+            var expectedItem = items[0];
+            Assert.AreEqual(expectedItem.SellIn, sellIn);
+            Assert.AreEqual(expectedItem.Quality, quality);
+        }
+
+        /// <summary>
+        /// "Backstage passes" quality increases by 1 when there are more than 10 days.
+        /// </summary>
+        /// <param name="sellIn">The range of sellIn.</param>
+        [Test]
+        public void GildedRose_UpdateQuality_IncreaseBackstagePassesProductQualityBy1_WhenSellInIsMoreThan10Days(
+            [Range(11, 20)] int sellIn)
+        {
+            //Arrange
+            var quality = 0;
+            var items = new List<Item>
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = quality }
+            };
+
+            //Act
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            //Assert
+            var expectedItem = items[0];
+            Assert.AreEqual(expectedItem.Quality, quality + 1);
+        }
+
+        /// <summary>
+        /// "Backstage passes" quality increases by 2 when there are 10 days or less.
+        /// </summary>
+        /// <param name="sellIn">The range of sellIn.</param>
+        public void GildedRose_UpdateQuality_IncreaseBackstagePassesProductQualityBy2_WhenSellInIsBetween10And6Days(
+            [Range(6, 10)] int sellIn)
+        {
+            //Arrange
+            var quality = 0;
+            var items = new List<Item>
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = quality }
+            };
+
+            //Act
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            //Assert
+            var expectedItem = items[0];
+            Assert.AreEqual(expectedItem.Quality, quality + 2);
+        }
+
+        /// <summary>
+        /// "Backstage passes" quality increases by 3 when there are 5 days or less.
+        /// </summary>
+        /// <param name="sellIn">The range of sellIn.</param>
+        [Test]
+        public void GildedRose_UpdateQuality_IncreaseBackstagePassesProductQualityBy3_WhenSellInIsBetween5And1Day(
+            [Range(1, 5)] int sellIn)
+        {
+            //Arrange
+            var quality = 0;
+            var items = new List<Item>
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = quality }
+            };
+
+            //Act
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            //Assert
+            var expectedItem = items[0];
+            Assert.AreEqual(expectedItem.Quality, quality + 3);
+        }
+
+        /// <summary>
+        /// "Backstage passes" quality drops to 0 after the concert.
+        /// </summary>
+        /// <param name="sellIn">A set of values for sellIn.</param>
+        /// <param name="quality">The range of quality.</param>
+        [Test]
+        public void GildedRose_UpdateQuality_BackstagePassesProductQualityDropsTo0_WhenConcertIsOver(
+            [Values(0, -5, -10)] int sellIn,
+            [Range(0, 50)] int quality)
+        {
+            //Arrange
+            var items = new List<Item>
+            {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = quality }
+            };
+
+            //Act
+            var gildedRose = new GildedRose(items);
+            gildedRose.UpdateQuality();
+
+            //Assert
+            var expectedItem = items[0];
+            Assert.AreEqual(expectedItem.Quality, 0);
+        }
     }
 }
